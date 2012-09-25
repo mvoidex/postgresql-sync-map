b0fc3c2cd24b0d3b0689f55c04d895bf16b2c15b
Alexandr Ruchkin <voidex@live.com>
2012-09-26 00:42:26 +0400

    Applying conditions only to affected models

--- src/Database/PostgreSQL/Report/Xlsx.hs
+++ src/Database/PostgreSQL/Report/Xlsx.hs
@@ -63,13 +63,15 @@ reportDeclaration f = do
     return $ zip kTexts eTexts
 
 generateReport :: Syncs -> [ReportFunction] -> [(T.Text, T.Text)] -> [T.Text] -> [T.Text] -> TIO [[FieldValue]]
-generateReport ss funs m conds orders = generate rptOrdered ss funs where
+generateReport ss funs m conds orders = generate rpt' ss funs where
     m' = map T.unpack $ map snd m
     flds = map report m'
-    additionalConds = map (condition . T.unpack) conds
-    rpt = fromMaybe (error $ "Unable to create report: " ++ show m) $ mconcat $ flds ++ additionalConds
-    rptOrdered = rpt `mappend` (mconcat . filter inTables . mapMaybe (orderBy . T.unpack) $ orders) where
-        inTables r = not $ null $ intersect (reportModels rpt) (reportModels r)
+    rpt = fromMaybe (error $ "Unable to create report: " ++ show m) $ mconcat $ flds
+    conds' = mapMaybe (condition . T.unpack) $ conds
+    orders' = mapMaybe (orderBy . T.unpack) $ orders
+
+    rpt' = rpt `mappend` (mconcat . filter inTables $ conds' ++ orders')
+    inTables r = not $ null $ intersect (reportModels rpt) (reportModels r)
 
 saveReport :: (MonadLog m, MonadIO m) => FilePath -> [T.Text] -> [[FieldValue]] -> m ()
 saveReport f ts fs = liftIO getCurrentTimeZone >>= saveReport' where
